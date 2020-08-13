1c4939a8adac325bda993896484d8b10cf5093d5
Dmitry Petrouk <dpetrouk@gmail.com>
2020-08-13 19:04:16 +0300

    create tests

---
+++ __tests__/index.test.js
@@ -0,0 +1,16 @@
+import generateDifference from '../src/generateDifference.js';
+
+const filepath1 = './__tests__/files/f1.json';
+const filepath2 = './__tests__/files/f2.json';
+const result = `{
+  - follow: false
+    host: hexlet.io
+  - proxy: 123.234.53.22
+  - timeout: 50
+  + timeout: 20
+  + verbose: true
+}`;
+
+test('genDiff', () => {
+  expect(generateDifference(filepath1, filepath2)).toEqual(result);
+});
