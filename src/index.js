22e5cf078ec52bbb46a0678c9896c9fd3f28f7ff
Dmitry Petrouk <dpetrouk@gmail.com>
2020-08-14 13:29:58 +0300

    rename generateDifference.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,33 @@
-import program from 'commander';
-import generateDifference from './generateDifference.js';
+import fs from 'fs';
+import _ from 'lodash';
 
-const runProgram = () => {
-  program
-    .version('0.0.1')
-    .description('Compares two configuration files and shows a difference.')
-    .option('-f, --format [type]', 'output format')
-    .arguments('<filepath1> <filepath2>')
-    .action((filepath1, filepath2) => console.log(generateDifference(filepath1, filepath2)));
-
-  return program;
+const getEntries = (filepath) => {
+  const str = fs.readFileSync(filepath);
+  const obj = JSON.parse(str, 'utf8');
+  const entries = Object.entries(obj);
+  return entries;
 };
 
-export default runProgram;
+const format = (array, mark = ' ') => array.map((entry) => `  ${mark} ${entry.join(': ')}`);
+
+const generateDifference = (filepath1, filepath2) => {
+  const entries1 = getEntries(filepath1);
+  const entries2 = getEntries(filepath2);
+
+  const unchanged = _.intersectionWith(entries1, entries2, _.isEqual);
+  const deleted = _.differenceWith(entries1, unchanged, _.isEqual);
+  const added = _.differenceWith(entries2, unchanged, _.isEqual);
+
+  const formattedDiff = _.sortBy(
+    [
+      ...format(unchanged),
+      ...format(deleted, '-'),
+      ...format(added, '+'),
+    ],
+    (entry) => entry.slice(4, entry.indexOf(':')),
+  ).join('\n');
+
+  return `{\n${formattedDiff}\n}`;
+};
+
+export default generateDifference;
