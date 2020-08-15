2f1c50293600611e79bf8a82bbb0c1ed096852ec
Dmitry Petrouk <dpetrouk@gmail.com>
2020-08-15 15:09:39 +0300

    add tests for comparing flat ini

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -3,12 +3,13 @@ import { dirname } from 'path';
 import path from 'path';
 import generateDifference from '../src/index.js';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const getFixturesPath = (filename) => {
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = dirname(__filename);
+  return path.join(__dirname, '..', '__fixtures__', filename)
+};
 
-const getFixturesPath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
-
-const result = `{
+const expected = `{
   - follow: false
     host: hexlet.io
   - proxy: 123.234.53.22
@@ -17,14 +18,12 @@ const result = `{
   + verbose: true
 }`;
 
-test('compare flat json files', () => {
-  const filepath1 = getFixturesPath('f1.json');
-  const filepath2 = getFixturesPath('f2.json');
-  expect(generateDifference(filepath1, filepath2)).toEqual(result);
-});
-
-test('compare flat yaml files', () => {
-  const filepath1 = getFixturesPath('f1.yml');
-  const filepath2 = getFixturesPath('f2.yml');
-  expect(generateDifference(filepath1, filepath2)).toEqual(result);
+test.each([
+  ['f1.json', 'f2.json'],
+  ['f1.yml', 'f2.yml'],
+  ['f1.ini', 'f2.ini'],
+])('compare flat files (json, yaml, ini)', (filename1, filename2) => {
+  const filepath1 = getFixturesPath(filename1);
+  const filepath2 = getFixturesPath(filename2);
+  expect(generateDifference(filepath1, filepath2)).toEqual(expected);
 });
