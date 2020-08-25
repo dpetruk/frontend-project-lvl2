bb1459c8f5e4923411eacec463c4067812dbb151
Dmitry Petrouk <dpetrouk@gmail.com>
2020-08-25 16:12:47 +0300

    add default formatter for recursive comparison

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import program from 'commander';
-import generateDifference from './index.js';
+import genDiff from './index.js';
 
 const runApp = () => {
   program
     .version('0.0.1')
     .description('Compares two configuration files and shows a difference.')
-    .option('-f, --format [type]', 'output format')
+    .option('-f, --format [type]', 'output format', 'stylish')
     .arguments('<filepath1> <filepath2>')
-    .action((filepath1, filepath2) => console.log(generateDifference(filepath1, filepath2)));
+    .action((filepath1, filepath2) => console.log(genDiff(filepath1, filepath2, program.format)));
 
   program.parse(process.argv);
 };
