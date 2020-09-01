d79b98a0ae70cc0258c0a19f218d62da29328460
Dmitry Petrouk <dpetrouk@gmail.com>
2020-09-01 13:48:38 +0300

    undo ini parsing fixing, change num format in input

--- src/parsers.js
+++ src/parsers.js
@@ -1,20 +1,8 @@
 import YAML from 'js-yaml';
 import ini from 'ini';
-import _ from 'lodash';
-
-const getValidValue = (value) => (/^[0-9]+$/.test(value) ? Number(value) : value);
-
-const fixNumParsing = (obj) => {
-  const keys = Object.keys(obj);
-  return keys
-    .reduce((acc, key) => {
-      const value = getValidValue(obj[key]);
-      return { ...acc, [key]: _.isPlainObject(value) ? fixNumParsing(value) : value };
-    }, {});
-};
 
 const parsers = {
-  ini: (string) => fixNumParsing(ini.parse(string)),
+  ini: (string) => ini.parse(string),
   json: (string) => JSON.parse(string),
   yaml: (string) => YAML.safeLoad(string),
   yml: (string) => YAML.safeLoad(string),
