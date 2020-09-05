e5603580bbad24648d9a7ad5ee913f2184c2db15
Dmitry Petruk <30505853+dpetruk@users.noreply.github.com>
2020-09-05 12:12:19 +0300

    fix duplication

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -10,30 +10,19 @@ const getFixturesPath = (filename) => {
   return path.join(__dirname, '..', '__fixtures__', filename)
 };
 
-const expected_stylish = fs.readFileSync(getFixturesPath(`expected_stylish`), 'utf8');
-const expected_plain = fs.readFileSync(getFixturesPath(`expected_plain`), 'utf8');
-const expected_json = fs.readFileSync(getFixturesPath(`expected_json`), 'utf8');
-
-const json1 = getFixturesPath('f1.json');
-const json2 = getFixturesPath('f2.json');
-const yaml1 = getFixturesPath('f1.yml');
-const yaml2 = getFixturesPath('f2.yaml');
-const ini1 = getFixturesPath('f1.ini');
-const ini2 = getFixturesPath('f2.ini');
-
 test.each([
-  ['f1.json', 'f2.json', 'stylish'],
-  ['f1.yml', 'f2.yaml', 'stylish'],
-  ['f1.ini', 'f2.ini', 'stylish'],
-  ['f1.json', 'f2.json', 'plain'],
-  ['f1.yml', 'f2.yaml', 'plain'],
-  ['f1.ini', 'f2.ini', 'plain'],
-  ['f1.json', 'f2.json', 'json'],
-  ['f1.yml', 'f2.yaml', 'json'],
-  ['f1.ini', 'f2.ini', 'json'],
-])(`compare deep files (%s, %s with %s)`, (filename1, filename2, formatter) => {
-  const filepath1 = getFixturesPath(filename1);
-  const filepath2 = getFixturesPath(filename2);
+  ['json', 'stylish'],
+  ['yml', 'stylish'],
+  ['ini', 'stylish'],
+  ['json', 'plain'],
+  ['yml', 'plain'],
+  ['ini', 'plain'],
+  ['json', 'json'],
+  ['yml', 'json'],
+  ['ini', 'json'],
+])(`compare deep files (%s, %s with %s)`, (inputExt, formatter) => {
+  const filepath1 = getFixturesPath(`f1.${inputExt}`);
+  const filepath2 = getFixturesPath(`f2.${inputExt}`);
 
   const expected = fs.readFileSync(getFixturesPath(`expected_${formatter}`), 'utf8');
 
