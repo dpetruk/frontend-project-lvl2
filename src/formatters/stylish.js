2befa7b6c9264486ec91bef2a182ca079e0358bf
Dmitry Petrouk <dpetrouk@gmail.com>
2020-09-19 11:51:07 +0300

    refactor: formatting for case without type, naming

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -9,7 +9,7 @@ const getList = (obj) => {
   const list = Object.entries(obj)
     .map((entry) => {
       const [key, value] = entry;
-      return { key, type: 'embedded in value', value };
+      return { key, value };
     });
 
   return list;
@@ -18,32 +18,34 @@ const getList = (obj) => {
 const formatListToStylish = (list, globalIndentSize = 0) => {
   const localIndentSize = globalIndentSize + indentShiftSize;
 
-  const getValidValue = (val) => {
+  const formatValue = (val) => {
     if (!_.isPlainObject(val)) {
       return val;
     }
-    const embeddedList = getList(val);
-    return formatListToStylish(embeddedList, localIndentSize);
+    return formatListToStylish(getList(val), localIndentSize);
   };
 
   const lines = list
     .map((entry) => {
       const { key, type } = entry;
+      if (!type) {
+        return `${spaceChar.repeat(localIndentSize)}${key}: ${formatValue(entry.value)}`;
+      }
 
       switch (type) {
         case 'removed': {
-          const removedValue = getValidValue(entry.value);
+          const removedValue = formatValue(entry.value);
           return `${_.padStart(minusMark, localIndentSize)}${key}: ${removedValue}`;
         }
 
         case 'added': {
-          const addedValue = getValidValue(entry.value);
+          const addedValue = formatValue(entry.value);
           return `${_.padStart(plusMark, localIndentSize)}${key}: ${addedValue}`;
         }
 
         case 'updated': {
-          const oldValue = getValidValue(entry.oldValue);
-          const newValue = getValidValue(entry.newValue);
+          const oldValue = formatValue(entry.oldValue);
+          const newValue = formatValue(entry.newValue);
 
           const oldEntry = `${_.padStart(minusMark, localIndentSize)}${key}: ${oldValue}`;
           const newEntry = `${_.padStart(plusMark, localIndentSize)}${key}: ${newValue}`;
@@ -52,7 +54,7 @@ const formatListToStylish = (list, globalIndentSize = 0) => {
         }
 
         case 'unchanged': {
-          const unchangedValue = getValidValue(entry.value);
+          const unchangedValue = formatValue(entry.value);
           return `${spaceChar.repeat(localIndentSize)}${key}: ${unchangedValue}`;
         }
 
@@ -61,9 +63,6 @@ const formatListToStylish = (list, globalIndentSize = 0) => {
           const parentValue = formatListToStylish(children, localIndentSize);
           return `${spaceChar.repeat(localIndentSize)}${key}: ${parentValue}`;
         }
-
-        case 'embedded in value':
-          return `${spaceChar.repeat(localIndentSize)}${key}: ${getValidValue(entry.value)}`;
 
         default: {
           throw new Error(`Unknown type '${type}'`);
